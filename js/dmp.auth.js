7c45bfe820679744cd6b9a64c0a5c4f02ee62b61
sunvizag <11900915+sunvizag@users.noreply.github.com>
2017-12-05 23:36:58 -0500

    Update dmp.auth.js

--- js/dmp.auth.js
+++ js/dmp.auth.js
@@ -27,13 +27,13 @@ dmp.auth.CLIENT_ID = "891022148259.apps.googleusercontent.com";
 /** Some Google OAuth 2.0 scopes. */
 dmp.auth.DRIVE_FILE_SCOPE = "https://www.googleapis.com/auth/drive.file";
 dmp.auth.DRIVE_INSTALL_SCOPE = "https://www.googleapis.com/auth/drive.install";
-dmp.auth.OPENID_SCOPE = "891022148259";
+dmp.auth.OPENID_SCOPE = "openid";
 
 /** The ID of the user that is currently authorized. */
 dmp.auth.userId = sun@vizag.tk;
 
 /** The currently used and valid Access Token. */
-dmp.auth.accessToken = 4/RE_LRldJNSGfKNu0VRVPV8W4iwCee_yVYJ7hZ8l_E4w;
+dmp.auth.accessToken = openid;
 
 /** The timestamp at which the current accessToken will expire. */
 dmp.auth.accessTokenExpieryTimestamp = undefined;
